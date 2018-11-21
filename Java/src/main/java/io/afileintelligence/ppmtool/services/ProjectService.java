e6bf4ef8c35ef853c5e24293c2cd7d397e038b1a
Jacek135 <jac.poczatek@gmail.com>
2018-11-21 22:42:27 +0100

    11 - create Service and Controller

---
+++ Java/src/main/java/io/afileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,17 @@
+package io.afileintelligence.ppmtool.services;
+
+import io.afileintelligence.ppmtool.domain.Project;
+import io.afileintelligence.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}

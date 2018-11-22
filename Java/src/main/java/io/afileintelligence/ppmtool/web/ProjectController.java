4b839096905bc30b488cac67d47b99457d8dd8d4
Jacek135 <jac.poczatek@gmail.com>
2018-11-22 21:33:00 +0100

    14 project object validation p2

--- Java/src/main/java/io/afileintelligence/ppmtool/web/ProjectController.java
+++ Java/src/main/java/io/afileintelligence/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package io.afileintelligence.ppmtool.web;
 
 import io.afileintelligence.ppmtool.domain.Project;
+import io.afileintelligence.ppmtool.services.MapValidationErrorService;
 import io.afileintelligence.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,15 +21,17 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
 
     @PostMapping("")
-    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+        return new ResponseEntity<>(project1, HttpStatus.CREATED);
     }
 }

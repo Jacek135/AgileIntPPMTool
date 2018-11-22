8925535dda0da2bd761f072e0cd159dae489ed20
Jacek135 <jac.poczatek@gmail.com>
2018-11-22 21:54:27 +0100

    16 project object validation p3

--- Java/src/main/java/io/afileintelligence/ppmtool/services/ProjectService.java
+++ Java/src/main/java/io/afileintelligence/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.afileintelligence.ppmtool.services;
 
 import io.afileintelligence.ppmtool.domain.Project;
+import io.afileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.afileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
 
-        //Logic
-
-        return projectRepository.save(project);
     }
 }

1540e78b0265946615907b1fdca0280acc2ed354
Jacek135 <jac.poczatek@gmail.com>
2018-11-24 15:03:50 +0100

    27 Styling

--- Java/ppmtool-react-client/src/components/Dashboard.js
+++ Java/ppmtool-react-client/src/components/Dashboard.js
@@ -6,9 +6,25 @@ import "bootstrap/dist/css/bootstrap.min.css"
 class Dashboard extends Component {
     render() {
         return (
-            <div>
-                <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
-                <ProjectItem/>
+            <div className="projects">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br/>
+                            <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                                Create a Project
+                            </a>
+                            <br/>
+                            <hr/>
+
+                            {/* Project Item Component*/}
+                            <ProjectItem/>
+                            {/*End of Project Item Component*/}
+
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
